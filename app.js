b2a13f02156aa3ea8df8abc743313865aab0d75f
Manuel Gozzi <manuel2006gozzi@gmail.com>
2026-03-05 14:40:01 +0100

    add: errorCalibrator

--- app.js
+++ app.js
@@ -3,16 +3,24 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const postsRouter = require("./router/posts");
+const ErrorCalibrator = require("./middlewares/ErrorCalibrator");
+const posts = require("./data/posts");
 
+// Index route
 app.get("/", (req, res) => {
   res.json({
-    result: "Benvenuti nel sito",
+    result: posts,
   });
 });
 
+// Public folder & json translate
 app.use(express.static("public"));
 app.use(express.json());
 
+// Routers
 app.use("/posts", postsRouter);
+
+// Error Calibrator
+app.use(ErrorCalibrator);
 
 app.listen(port, () => {});
